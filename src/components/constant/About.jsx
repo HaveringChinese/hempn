d985cc61941bdf4dda12b29a0e269cdba70cc495
Nate Conrad <nathanielrconrad@gmail.com>
2022-01-20 19:40:56 -0500

    structure about page with standin text and images

--- src/components/constant/About.jsx
+++ src/components/constant/About.jsx
@@ -1,7 +1,33 @@
 function About(){
 
 
-  return 
+  return (
+    <div className="about">
+
+        <main className="about-main">
+        <div className="inspiration">
+          <img src="https://www.fillmurray.com/640/360" alt="" className="inspiring-image" />
+          <p className="inspiring-words">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quisque egestas diam in arcu cursus euismod quis viverra. Sed vulputate odio ut enim blandit. Quis auctor elit sed vulputate mi sit amet mauris commodo. Purus sit amet volutpat consequat mauris nunc. Velit euismod in pellentesque massa placerat duis. Venenatis urna cursus eget nunc scelerisque viverra mauris in. Augue interdum velit euismod in pellentesque massa placerat duis ultricies. Tempus urna et pharetra pharetra massa massa ultricies. Mauris pharetra et ultrices neque ornare aenean euismod. Gravida arcu ac tortor dignissim convallis aenean et tortor. Id volutpat lacus laoreet non.
+          </p>
+        </div>
+        <div className="mission">
+          <img src="https://www.fillmurray.com/640/360" alt="" className="mission-image" />
+          <p className="mission-statement">
+          A diam maecenas sed enim ut sem viverra aliquet. Tellus in hac habitasse platea dictumst vestibulum rhoncus est. Congue eu consequat ac felis donec et odio pellentesque diam. Augue neque gravida in fermentum et sollicitudin ac orci. Consequat nisl vel pretium lectus. Elementum pulvinar etiam non quam lacus. Amet consectetur adipiscing elit ut. Enim facilisis gravida neque convallis. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra. Venenatis a condimentum vitae sapien pellentesque habitant.
+          </p>
+        </div>
+        <div className="strategy">
+          <img src="https://www.fillmurray.com/640/360" alt="" className="strategy-image" />
+          <p className="strategy-statement">
+          Metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Cum sociis natoque penatibus et magnis dis parturient montes. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. Dui nunc mattis enim ut tellus elementum sagittis vitae et. Bibendum est ultricies integer quis. Ut aliquam purus sit amet luctus venenatis. Velit egestas dui id ornare arcu odio ut sem nulla. Libero id faucibus nisl tincidunt eget nullam non. Amet risus nullam eget felis eget nunc. Senectus et netus et malesuada fames ac. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Nibh tortor id aliquet lectus. Iaculis nunc sed augue lacus viverra vitae congue. Senectus et netus et malesuada.
+          </p>
+        </div>
+        </main>
+
+
+    </div>
+  )
   
 }
 
