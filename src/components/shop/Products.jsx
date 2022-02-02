62e640c77c89151c5bebd572f6294078f7658a79
Nathaniel Conrad <nathanielrconrad@gmail.com>
2022-02-02 15:03:17 -0500

    add product images

--- src/components/shop/Products.jsx
+++ src/components/shop/Products.jsx
@@ -1,3 +1,15 @@
+import Mask from "../images/Black-Mask.jpg";
+import ChewToy from "../images/dogchewtoy.jpg";
+import Beanie from "../images/europeanslouchbeanie.jpg";
+import Tote from "../images/Farmer_tote__51179.jpg";
+import Tee from "../images/goodPlanetsT.jpeg";
+import Journal from "../images/hemp-journal.jpg";
+import Towels from "../images/Hemp-Kitchen-Towels-Group-800x800.png"
+import TP from "../images/Hemp-Toilet-Paper.jpg";
+import Cord from "../images/hempcord.jpg";
+import Wallet from "../images/slimelinewalletgreen.jpg";
+import Toiletry from "../images/Toiletry-Bag.jpg";
+
 
 const Products = [
   {
@@ -5,127 +17,89 @@ const Products = [
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: Mask
   },
   {
   key: 2,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: ChewToy
   },
   {
   key: 3,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: Beanie
   },
   {
   key: 4,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: Tote
   },
   {
   key: 5,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: Tee 
   },
   {
   key: 6,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: Journal
   }, 
   {
   key: 7,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: Towels
   },
   {
   key: 8,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: TP
   },
   {
   key: 9,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: Cord
   },
   {
   key: 10,
   name: "hemp (raw)",
   description: "Bulk industrial hemp.",
   price: "$.40/lb",
-  carbon: 0.15
+  carbon: 0.15,
+  source: Wallet
   },
   {
-  key: 11,
-  name: "hemp (raw)",
-  description: "Bulk industrial hemp.",
-  price: "$.40/lb",
-  carbon: 0.15
-  },
-  {
-  key: 12,
-  name: "hemp (raw)",
-  description: "Bulk industrial hemp.",
-  price: "$.40/lb",
-  carbon: 0.15
-  }, 
-  {
-  key: 13,
-  name: "hemp (raw)",
-  description: "Bulk industrial hemp.",
-  price: "$.40/lb",
-  carbon: 0.15
-  },
-  {
-  key: 14,
-  name: "hemp (raw)",
-  description: "Bulk industrial hemp.",
-  price: "$.40/lb",
-  carbon: 0.15
-  },
-  {
-  key: 15,
-  name: "hemp (raw)",
-  description: "Bulk industrial hemp.",
-  price: "$.40/lb",
-  carbon: 0.15
-  },
-  {
-  key: 16,
-  name: "hemp (raw)",
-  description: "Bulk industrial hemp.",
-  price: "$.40/lb",
-  carbon: 0.15
-  },
-  {
-  key: 17,
-  name: "hemp (raw)",
-  description: "Bulk industrial hemp.",
-  price: "$.40/lb",
-  carbon: 0.15
-  },
-  {
-  key: 18,
-  name: "hemp (raw)",
-  description: "Bulk industrial hemp.",
-  price: "$.40/lb",
-  carbon: 0.15
-  }
+    key: 11,
+    name: "hemp (raw)",
+    description: "Bulk industrial hemp.",
+    price: "$.40/lb",
+    carbon: 0.15,
+    source: Toiletry
+    }
 ];
 
 export default Products;
